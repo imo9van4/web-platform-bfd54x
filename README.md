db0d2b77d24589a40ace6c74921a4e337965cd1f
imo9van4 <vfarmer1@live.com>
2021-04-06 14:25:55 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-bfd54x
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-bfd54x)
---
+++ script.js
@@ -0,0 +1,245 @@
+var questions = 34;
+var questionsLeft = " [" + questions + " questions left]";
+
+var fname = prompt("Please type a woman's name" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var number = prompt("Please type a number" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var job = prompt("Successful occupation ending in -er" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var verb = prompt("Verb not ending in -ing" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var industry = prompt("A type of industry " + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var food = prompt("A smelly food" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var adjective = prompt("Adjective" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var verbprt = prompt("Verb ending in -s" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var adjective2 = prompt("Adjective" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var holidayadj = prompt("Holiday-themed adjective" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var dessert = prompt("Type of dessert" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var animal = prompt("Rare animal" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var color = prompt("Color" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var day = prompt("Day of the week" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var size = prompt("Size-related adjective" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var relative = prompt("Family relation/relative" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var winterAnimal = prompt("Animal associated with Winter" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var body = prompt("Body part" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var town = winterAnimal + "'s " + body;
+
+var activity = prompt("Expensive activity" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var city = prompt("Name of big city" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var verbing = prompt("Verb that ends in -ing" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var masculine = prompt("Masculine adjective" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var dog = prompt("A common name for a dog" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var manlyjob = prompt("Masculine occupation" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var nouner = prompt("Noun ending in -er" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var diffFood = prompt("Type of food" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var adjective3 = prompt("Adjective" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var verbpt = prompt("Verb in past-tense" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var time = prompt("An amount of time" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var mname = prompt("Man's name" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var romanticVerb = prompt("Romantic verb" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var verbpt2 = prompt("Verb past-tense" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var white = prompt("Something white" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+var mean = prompt("A mean name/an insult" + questionsLeft);
+questions -= 1;
+questionsLeft = " [" + questions + " questions left]";
+
+alert("All done. Ready for the message?");
+
+var sentence = fname + " is a " + number + "-year-old investment ";
+sentence += job + " with a thirst to " + verb + " herself in the world of ";
+sentence +=
+  industry +
+  ". She is a cutthroat workaholic who is constantly drinking espressos and eating " +
+  food +
+  " in order to meet her " +
+  adjective +
+  " deadlines. " +
+  fname +
+  " crosses who she has to cross, steps on who she has to step on, and " +
+  verbprt +
+  " whoever she needs to. All of her focus on work and herself has caused " +
+  fname +
+  " to overlook the importance of spending time with her " +
+  adjective2 +
+  " family at their annual Christmas Eve " +
+  holidayadj +
+  " town-wide " +
+  dessert +
+  "-walk to raise money for their " +
+  animal +
+  " ranch. <br>On " +
+  color +
+  " " +
+  day +
+  ", " +
+  fname +
+  "'s " +
+  size +
+  " " +
+  relative +
+  " called. " +
+  '"' +
+  fname +
+  '!" he/she said. "You have to come home to ' +
+  town +
+  ". The ranch is in trouble. We don't have enough money to " +
+  activity +
+  ' anymore!" <br>' +
+  fname +
+  ' scoffed. "I have a deadline," she said. "The ' +
+  animal +
+  " can wait. I don't want to leave " +
+  city +
+  " to go " +
+  verbing +
+  '." <br> "But the only other person willing to help us is ' +
+  masculine +
+  " " +
+  dog +
+  ", the town's " +
+  manlyjob +
+  " and volunteer " +
+  nouner +
+  " at the " +
+  diffFood +
+  ' kitchen."<br> ' +
+  fname +
+  " suddenly quieted. She had a long-harbored crush on " +
+  masculine +
+  " " +
+  dog +
+  " in her childhood and suddenly felt the urge to bake bread for him. She hopped on the first plane to " +
+  town +
+  '. "Where is your ticket?" demanded a(n) ' +
+  adjective3 +
+  ' stewardess.<br>"I have a deadline!" ' +
+  fname +
+  " shouted. As soon as the plane touched the ground, " +
+  fname +
+  " " +
+  verbpt +
+  " out the plane and ran to find " +
+  dog +
+  ". They crossed paths within " +
+  time +
+  '. "I saved the ' +
+  animal +
+  ' ranch," ' +
+  dog +
+  " said as his golden retriever, " +
+  mname +
+  ' jumped up next to him. <br> "Listen, I have a deadline," ' +
+  fname +
+  " said. \"So, let's " +
+  romanticVerb +
+  '."<br> "You read my mind," ' +
+  dog +
+  ' said. "Merry Christmas. Marry me."<br>"No," ' +
+  fname +
+  ' replied. "Merry Christmas. Marry me."<br> The two ' +
+  verbpt2 +
+  " as " +
+  white +
+  " began to fall from the sky. After that, " +
+  fname +
+  " wasn't such a(n) " +
+  mean +
+  " anymore. She moved back home and fell in love with Christmas and never had a deadline again.";
+
+document.write(sentence);
